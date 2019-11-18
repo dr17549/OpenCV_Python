086eae965740146c72e793086335a2a97da97e99
Dhammatorn Riewcharoon <dhammatornriewcharoon@eduroam-165-240.nomadic.bris.ac.uk>
2019-11-18 13:13:15 +0000

    cleaned up line

--- line.py
+++ line.py
@@ -5,7 +5,6 @@ import math
 
 np.set_printoptions(threshold=sys.maxsize)
 
-# read grey image
 img = cv2.imread('coins1.png',0)
 img_output = cv2.imread('coins1.png',1 )
 height, width = img.shape
@@ -20,14 +19,13 @@ hough_space_output = np.zeros((height,width),np.float32)
 hough_space_max_r = np.zeros((height,width),np.float32)
 hough_space_gradient_threshold = 90
 
-# Cal DX
-# ---------------------------------------------------
+
 def convolution(img,kernel):
 
     image_convoluted = np.zeros((height, width), np.float32)
-    kernelRadiusX = 1
-    kernelRadiusY = 1
-    img_padded = cv2.copyMakeBorder(img, kernelRadiusX, kernelRadiusX, kernelRadiusY, kernelRadiusY, cv2.BORDER_REPLICATE)
+    kernelX = 1
+    kernelY = 1
+    img_padded = cv2.copyMakeBorder(img, kernelX, kernelX, kernelY, kernelY, cv2.BORDER_REPLICATE)
     for i in range(0,height):
         for j in range(0,width):
             cov = 0
@@ -38,8 +36,6 @@ def convolution(img,kernel):
 
     return image_convoluted
 
-# Normalizer dor DX
-# ---------------------------------------------------
 def normalize(image_dx):
 
     min = image_dx.min()
@@ -63,62 +59,53 @@ def sobel(img,dx,dy):
 
     return image_dx,image_dy,gradient_magnitude,gradient_angle
 
-# image_dx = convolution(img,dx)
-# image_dy = convolution(img,dy)
-# image_dx = normalize(image_dx)
-# image_dy = normalize(image_dy)
+
+def circle_detection_hough_space(gradient_magnitude,gradient_angle, hough_space_gradient_threshold):
+
+    hough_space = np.zeros((height, width, radius), np.int32)
+    hough_space_output = np.zeros((height, width), np.float32)
+    hough_space_max_r = np.zeros((height, width), np.float32)
+
+    for i in range(height):
+        for j in range(width):
+            if(gradient_magnitude[i][j] > hough_space_gradient_threshold):
+                for radi in range(radius):
+                    x0 = i + int(radi * math.sin(gradient_angle[i][j]))
+                    y0 = j + int(radi * math.cos(gradient_angle[i][j]))
+                    if x0 >= 0 and y0 >= 0 and x0 < height and y0 < width:
+                        hough_space[x0][y0][radi] += 1
+
+                    x1 = i - int(radi * math.sin(gradient_angle[i][j]))
+                    y1 = j - int(radi * math.cos(gradient_angle[i][j]))
+                    if x1 >= 0 and y1 >= 0 and x1 < height and y1 < width:
+                        hough_space[x1][y1][radi] += 1
+
+    for i in range(0,height):
+        for j in range(0,width):
+            for radi in range(radius):
+                hough_space_output[i][j] = hough_space[i][j][radi] + hough_space_output[i][j]
+
+    for i in range(0,height):
+        for j in range(0,width):
+            max = 0
+            max_radi = 1
+            for radi in range(radius):
+                if hough_space[i][j][radi] > max:
+                    max = hough_space[i][j][radi]
+                    max_radi = radi
+            hough_space_max_r[i][j] = max_radi
+
+    for i in range(0,height):
+        for j in range(0,width):
+            if hough_space_output[i][j] > 20:
+                color = (255,0,0)
+                cv2.circle(img_output, (j,i), hough_space_max_r[i][j], color, 1)
+
 
 
 image_dx,image_dy,gradient_magnitude,gradient_angle = sobel(img,dx,dy)
-# gradient_magnitude = normalize(gradient_magnitude)
-# gradient_angle = normalize(gradient_angle)
-
-
-# Calculate the Hough Space Circle Detection
-# ---------------------------------------------------
-for i in range(height):
-    for j in range(width):
-        if(gradient_magnitude[i][j] > hough_space_gradient_threshold):
-            for radi in range(radius):
-                x0 = i + int(radi * math.sin(gradient_angle[i][j]))
-                y0 = j + int(radi * math.cos(gradient_angle[i][j]))
-                if x0 >= 0 and y0 >= 0 and x0 < height and y0 < width:
-                    hough_space[x0][y0][radi] += 1
-
-                x1 = i - int(radi * math.sin(gradient_angle[i][j]))
-                y1 = j - int(radi * math.cos(gradient_angle[i][j]))
-                if x1 >= 0 and y1 >= 0 and x1 < height and y1 < width:
-                    hough_space[x1][y1][radi] += 1
-
-for i in range(0,height):
-    for j in range(0,width):
-        for radi in range(radius):
-            hough_space_output[i][j] = hough_space[i][j][radi] + hough_space_output[i][j]
-
-for i in range(0,height):
-    for j in range(0,width):
-        max = 0
-        max_radi = 1
-        for radi in range(radius):
-            if hough_space[i][j][radi] > max:
-                max = hough_space[i][j][radi]
-                max_radi = radi
-        hough_space_max_r[i][j] = max_radi
-
-for i in range(0,height):
-    for j in range(0,width):
-        if hough_space_output[i][j] > 15:
-            color = (255,0,0)
-            cv2.circle(img_output, (j,i), hough_space_max_r[i][j], color, 1)
-# ---------------------------------------------------
-
-
-
-# print(image_dx)
+circle_detection_hough_space(gradient_magnitude,gradient_angle, hough_space_gradient_threshold)
 cv2.imwrite('output.png',img_output)
-# cv2.imwrite('output_mag.png',gradient_magnitude)
-# cv2.imwrite('output_angle.png',gradient_angle)
 
-# print(gradient_magnitude)
 
 
